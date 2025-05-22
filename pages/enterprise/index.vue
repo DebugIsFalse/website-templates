d3a61e69cc6ea6660552d2c055a62e027918f8e9
DebugIsFalse <dm511418503@gmail.com>
2025-05-22 17:39:41 +0800

    feauture: add router and tailwindcss

---
+++ pages/enterprise/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full h-full">
+      <div class="w-full h-full bg-red-500">
+          <h1>Hello World</h1>
+      </div>
+  </div>
+</template>
+<script lang="ts" setup>
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full h-full">
+    <a href="/blog">blog</a>
+    <a href="/enterprise">enterprise</a>
+  </div>
+</template>
+<script setup>
+definePageMeta({
+  // redirect: '/blog/home'
+})
+</script> 
